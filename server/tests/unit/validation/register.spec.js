38aa7c5a3b3479c5ddab089603a97c86e51aaf0f
Alex <sd@alexdus.com>
2019-01-05 22:34:37 +0300

    Add tests for validateEmail

--- server/tests/unit/validation/register.spec.js
+++ server/tests/unit/validation/register.spec.js
@@ -1,4 +1,5 @@
 import validators from '../../../validators';
+import { User } from '../../../database/models';
 
 const { RegisterUserValidator } = validators;
 
@@ -45,5 +46,41 @@ describe('RegisterUserValidator class', () => {
 
       expect(validator.errors).toContain('The password must be longer than 5 characters.');
     });
+  });
+
+  describe('Validate Email Test', () => {
+    test('Should add an error to the errors array if no email provided ', async () => {
+      const validator = new RegisterUserValidator({ email: '' });
+
+      await validator.validateEmail();
+
+      expect(validator.errors).toContain('The email is required.');
+    });
+
+    test('Should add an error to the errors array if invalid email provided', async () => {
+      const validator = new RegisterUserValidator({ email: 'sd@alexdus' });
+
+      await validator.validateEmail();
+
+      expect(validator.errors).toContain('The email must be a valid email address.');
+    });
+
+    test('Should add an error to errors array if the email is already in db', async () => {
+      await User.destroy({ where: {} });
+
+      const email = 'sd@alexdus.com';
+
+      await User.create({
+        name: 'Alex',
+        password: 'password',
+        email,
+      });
+
+      const validator = new RegisterUserValidator({ email });
+
+      await validator.validateEmail();
+
+      expect(validator.errors).toContain('A user with this email already exists.');
+    })
   });
 });
