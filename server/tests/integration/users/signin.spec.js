a2d866d5db08850ec799aa23a36d76fe298f3441
Alex <sd@alexdus.com>
2019-01-06 15:16:13 +0300

    Add integration tests for sign in email/password validation

--- server/tests/integration/users/signin.spec.js
+++ server/tests/integration/users/signin.spec.js
@@ -4,9 +4,13 @@ import { User } from '../../../database/models';
 import app from '../../../index';
 
 describe('User Login Tests', () => {
-  test('Should allow user to login and get JWT', async () => {
-    await User.destroy({ where: {} });
+  const SIGNIN_ENDPOINT = '/api/v1/users/signin';
 
+  beforeEach(async () => {
+    await User.destroy({ where: {} });
+  });
+
+  test('Should allow user to login and get JWT', async () => {
     const userData = {
       name: 'Alex Dus',
       password: 'password',
@@ -20,7 +24,7 @@ describe('User Login Tests', () => {
     });
 
     const { status, body } = await supertest(app)
-      .post('/api/v1/users/signin')
+      .post(SIGNIN_ENDPOINT)
       .send({
         email: userData.email,
         password: userData.password,
@@ -29,5 +33,47 @@ describe('User Login Tests', () => {
     expect(status).toBe(200);
     expect(body.data.access_token).toBeTruthy();
     expect(body.data.user.email).toBe(userData.email);
+  });
+
+  test('Should return an error if password is not passed', async () => {
+    const user = {
+      password: '',
+      email: 'sd@alexdus.com',
+    };
+
+    const { status, body } = await supertest(app)
+      .post(SIGNIN_ENDPOINT)
+      .send(user);
+
+    expect(status).toBe(422);
+    expect(body.data.errors).toContain('The password is required.');
+  });
+
+  test('Should return an error if passed email is not valid', async () => {
+    const user = {
+      email: 'sd@alexdus',
+      password: 'password',
+    };
+
+    const { status, body } = await supertest(app)
+      .post(SIGNIN_ENDPOINT)
+      .send(user);
+
+    expect(status).toBe(422);
+    expect(body.data.errors).toContain('The email must be a valid email address.');
+  });
+
+  test('Should retun an error if email is not passed', async () => {
+    const user = {
+      email: '',
+      password: 'password',
+    };
+
+    const { status, body } = await supertest(app)
+      .post(SIGNIN_ENDPOINT)
+      .send(user);
+
+    expect(status).toBe(422);
+    expect(body.data.errors).toContain('The email is required.');
   });
 });
